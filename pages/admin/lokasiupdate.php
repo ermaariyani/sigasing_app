c4810cb529e5de9002b0fd4d3306ae63f164c7ed
ermaariyani <ermaariyani63@gmail.com>
2022-02-21 23:43:53 +0800

    Membuat Halaman Ubah Data

---
+++ pages/admin/lokasiupdate.php
@@ -0,0 +1,97 @@
+<?php
+if (isset($_GET['id'])) {
+    $database = new Database();
+    $db = $database->getConnection();
+
+    $findSQL = "SELECT * FROM lokasi WHERE id=?";
+    $stmt = $db->prepare($findSQL);
+    $stmt->bindParam(1, $_GET['id']);
+    $stmt->execute();
+    $row = $stmt->fetch();
+
+    if (isset($row['id'])) {
+?>
+        <section class="content-header">
+            <?php
+            if (isset($_POST['button_create'])) {
+
+                $database = new Database();
+                $db = $database->getConnection();
+
+                $validateSQL = "SELECT * FROM lokasi WHERE nama_lokasi=?";
+                $stmt = $db->prepare($validateSQL);
+                $stmt->bindParam(1, $_POST['nama_lokasi']);
+                $stmt->execute();
+
+                if ($stmt->rowCount() > 0) {
+            ?>
+                    <div class="alert alert-danger alert-dismissable">
+                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+                        <h5><i class="fas fa-ban"></i>Gagal</h5>
+                        Nama Lokasi sama sudah ada
+                    </div>
+            <?php
+                } else {
+                    $insertSQL = "INSERT INTO lokasi SET nama_lokasi=?";
+
+                    $stmt = $db->prepare($insertSQL);
+                    $stmt->bindParam(1, $_POST['nama_lokasi']);
+                    if ($stmt->execute()) {
+                        $_SESSION['hasil'] = true;
+                        $_SESSION['pesan'] = "Berhasil Simpan Data";
+                    } else {
+                        $_SESSION['hasil'] = false;
+                        $_SESSION['pesan'] = "Gagal Simpan Data";
+                    }
+
+
+
+                    echo "<meta http-equiv='refresh' content='0;url=?page=lokasiread'";
+                }
+            }
+            ?>
+            <div class="container-fluid">
+                <div class="row mb2">
+                    <div class="col-sm-6">
+                        <h1>Ubah Data Lokasi</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="?page=home"> Home </a></li>
+                            <li class="breadcrumb-item"><a href="?page=lokasiread">Lokasi</a></li>
+                            <li class="breadcrumb-item active">Ubah Data</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="content">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Ubah Lokasi</h3>
+                </div>
+                <div class="card-body">
+                    <form method="POST">
+                        <div class="form-group">
+                            <label for="nama_lokasi">Nama Lokasi*</label>
+                            <input type="hidden" class="form-cobtrol" name="id" value="<?php echo $row['id'] ?>">
+                            <input type="text" class="form-control" name="nama_lokasi" value="<?php echo $row['nama_lokasi'] ?>">
+                        </div>
+                        <a href="?page=lokasiread" class="btn btn-danger btn-sm float-right">
+                            <i class="fa fa-times"></i> Batal
+                        </a>
+                        <button type="submit" name="button_update" class="btn btn-success btn-sm float-right">
+                            <i class="fa fa-save"></i> Simpan
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </section>
+<?php
+    } else {
+        echo "<meta http-equiv='refresh' content='0;url=?page=lokasiread'";
+    }
+} else {
+    echo "<meta http-equiv='refresh' content='0;url=?page=lokasiread'";
+}
